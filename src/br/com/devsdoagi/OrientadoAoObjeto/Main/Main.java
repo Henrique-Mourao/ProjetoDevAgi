739ccd695ab5b30719a6f69b4cc918586e55d9d6
unknown <dev40@NB-UNIAGI-40.AGIPLAN.LOCAL>
2025-08-15 11:58:35 -0300

    OrientadoObjetoProduto

--- src/br/com/devsdoagi/OrientadoAoObjeto/Main/Main.java
+++ src/br/com/devsdoagi/OrientadoAoObjeto/Main/Main.java
@@ -1,9 +1,11 @@
 package br.com.devsdoagi.OrientadoAoObjeto.Main;
 import br.com.devsdoagi.OrientadoAoObjeto.Classes.Aluno;
 import br.com.devsdoagi.OrientadoAoObjeto.Classes.Carro;
+import br.com.devsdoagi.OrientadoAoObjeto.Classes.Produto
 
 public class Main {
   public static void main(String[] args) {
+
       /*
         Aluno a1 = new Aluno("Henrique", 4, 10);
         double mediaHenrique = a1.calcularMedia();
@@ -14,8 +16,7 @@ public class Main {
       meuCarro.mostrarInformacoes();
       meuCarro.acelerar();
       meuCarro.frear();*/
-
-      Carro meuCarro = new Carro();
+      /* Carro meuCarro = new Carro();
 
       meuCarro.marca = "Toyota";
       meuCarro.modelo = "Corolla";
@@ -23,6 +24,15 @@ public class Main {
 
       meuCarro.mostrarInformacoes();
       meuCarro.acelerar();
-      meuCarro.frear();
+      meuCarro.frear();*/
+      Produto p1 = new Produto();
+
+      p1.exibirDados();
+      p1.nome="Danone";
+      p1.preco=540;
+      p1.adicionarEstoque(10);
+      p1.removerEstoque(5);
+      p1.exibirDados();
+
   }
 }
