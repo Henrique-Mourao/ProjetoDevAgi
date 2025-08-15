4bc27e65279f6cdf023590f845bb44c1a789998c
unknown <dev40@NB-UNIAGI-40.AGIPLAN.LOCAL>
2025-08-15 08:18:30 -0300

    Feat ProjecaoCenariosMatriz

---
+++ src/br/com/devsdoagi/Vetores/RendaMensal.java
@@ -0,0 +1,24 @@
+package br.com.devsdoagi.Vetores;
+
+public class RendaMensal {
+    public static double[] calcularRendaMensal(double[][] matrizCenarios, double[] taxasAnuais, int expectativaVida) {
+        int numeroCenarios = matrizCenarios.length;
+        double[] rendasMensais = new double[numeroCenarios];
+
+        for (int i = 0; i < numeroCenarios; i++) {
+            double[] cenarios = matrizCenarios[i]; // dados do cenário i
+            double somaTotal = 0;
+
+            int anosDisponiveis = Math.min(expectativaVida, Math.min(cenarios.length, taxasAnuais.length));
+
+            for (int ano = 0; ano < anosDisponiveis; ano++) {
+                // Aplica taxa anual se aplicável
+                somaTotal += cenarios[ano] * taxasAnuais[ano];
+            }
+
+            rendasMensais[i] = somaTotal / (expectativaVida * 12); // média mensal
+        }
+
+        return rendasMensais;
+    }
+}
