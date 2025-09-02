fc57c2697fd5ef9b69b8dcbd93c86399c20287d3
unknown <dev40@NB-UNIAGI-40.AGIPLAN.LOCAL>
2025-09-02 08:48:38 -0300

    Adicionando Hackthon Carteira

---
+++ src/br/com/devsdoagi/JavaBasico/Hackathons/BancoDigital/Main/Main.java
@@ -0,0 +1,4 @@
+package br.com.devsdoagi.JavaBasico.Hackathons.BancoDigital.Main;
+
+public class Main {
+}
---
+++ src/br/com/devsdoagi/JavaBasico/Hackathons/BancoDigital/SubClasse/Transacao.java
@@ -0,0 +1,4 @@
+package br.com.devsdoagi.JavaBasico.Hackathons.BancoDigital.SubClasse;
+
+public class Transacao {
+}
